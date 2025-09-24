dcd8e1802efbae84ecc04235faafb8d9aa5a638f
mi <milena.kaminski.ifpr@gmail.com>
2025-09-24 16:16:34 -0300

    Add files via upload

---
+++ item.1.js
@@ -0,0 +1,14 @@
+var a=[4, 8, 3, 5, 9] /*Criando um vetor ja preenchido */
+
+/*Exibindo o array de inteiros*/
+alert(a)
+/*Exibindo um elemento do array*/
+alert(a[4])
+/*Alterar um valor*/
+a[0]=7
+alert(a)
+/*Percorrendo o vetor usando laco*/
+for(var i=0; i<a.length; i++)
+{
+    document.write("<h2>"+a[i]+"</h2>")
+}
---
+++ item3.js
@@ -0,0 +1,13 @@
+var frutas=[]
+var f
+do{
+    f=prompt("Digite a fruta")
+    frutas.push(f)
+
+}while(f!=null)
+
+alert(frutas)
+for( var i=0; i<frutas.length;i++)
+{
+    document.write("<h2>"+frutas[i]+"</h2>")
+}
